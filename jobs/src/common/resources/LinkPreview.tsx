358cd663d77b69790ecddb41267c1494dd717639
Tosin Kuye <tkuye77@gmail.com>
2022-04-06 22:07:11 -0600

    validURL

--- jobs/src/common/resources/LinkPreview.tsx
+++ jobs/src/common/resources/LinkPreview.tsx
@@ -8,6 +8,16 @@ interface LinkPreviewProps {
 	description?: string;
 	image?: string;
 }
+function validURL(str: string) {
+	var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+	  '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+	  '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+	  '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+	  '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+	  '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+	return !!pattern.test(str);
+  }
+
 
 const LinkPreview: React.FC<LinkPreviewProps> = ({url, title, description, image}) => {
 		let descriptionLength = description?.length || 60;
@@ -44,7 +54,7 @@ const LinkPreview: React.FC<LinkPreviewProps> = ({url, title, description, image
           </span>
         )}
         <div className={`${styles.SiteDetails} ${styles.Secondary}`}>
-          {url && <span>{ new URL(url)?.hostname + " •"|| ''}</span>}
+          {url && <span>{ validURL(url)? new URL(url)?.hostname + " •": ''}</span>}
         </div>
       </div>
 		</div></a></Link>);
